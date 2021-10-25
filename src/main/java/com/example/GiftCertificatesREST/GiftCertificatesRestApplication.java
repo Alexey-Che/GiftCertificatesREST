760295dd880a7da7e5689940483897158b9936d7
Алексей Коротких <lexa.36484@gmail.com>
2021-10-25 22:24:30 +0500

    init

--- src/main/java/com/example/GiftCertificatesREST/GiftCertificatesRestApplication.java
+++ src/main/java/com/example/GiftCertificatesREST/GiftCertificatesRestApplication.java
@@ -1,9 +1,7 @@
 package com.example.GiftCertificatesREST;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GiftCertificatesRestApplication {
@@ -11,10 +9,4 @@ public class GiftCertificatesRestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GiftCertificatesRestApplication.class, args);
 	}
-
-	@Bean
-	public ModelMapper modelMapper() {
-		return new ModelMapper();
-	}
-
 }
