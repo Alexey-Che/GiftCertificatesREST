b8ceec3eb49c7df41e4637b2e55744512a9f0867
Алексей Коротких <lexa.36484@gmail.com>
2021-11-23 19:08:50 +0500

    init

---
+++ src/test/java/com/example/GiftCertificatesREST/service/CertificateServiceImplTest.java
@@ -0,0 +1,40 @@
+package com.example.GiftCertificatesREST.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CertificateServiceImplTest {
+
+    @Test
+    void createGiftCertificate() {
+    }
+
+    @Test
+    void getAllCertificates() {
+    }
+
+    @Test
+    void getGiftCertificateBySubstring() {
+    }
+
+    @Test
+    void deleteGiftCertificate() {
+    }
+
+    @Test
+    void getGiftCertificateById() {
+    }
+
+    @Test
+    void updateGiftCertificate() {
+    }
+
+    @Test
+    void getGiftCertificatesByTagId() {
+    }
+
+    @Test
+    void getSortedGiftCertificatesBy() {
+    }
+}
---
+++ src/test/java/com/example/GiftCertificatesREST/service/TagServiceImplTest.java
@@ -0,0 +1,24 @@
+package com.example.GiftCertificatesREST.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TagServiceImplTest {
+
+    @Test
+    void createTag() {
+    }
+
+    @Test
+    void getAllTags() {
+    }
+
+    @Test
+    void deleteTag() {
+    }
+
+    @Test
+    void getTagById() {
+    }
+}
